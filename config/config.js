6cf611a6277192a5a60a405940860ce113c41024
Andrew Crawford <andrew@crawford.works>
2016-02-08 15:59:20 -0700

    blah

---
+++ config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'port': 3000,
+    'database_URL': '127.0.0.1',
+    'database_user': 'root',
+    'database_password': 'password',
+    'database': 'hobblit',
+    'database_charset': 'utf8',
+    'secret': 'F:JDSF:LKJSDFBUASFLKJ'
+};
